1ec363e0624c284df44dac8094a95004e37ccac4
Ryan Haack <ryan@haackr.dev>
2023-08-27 11:50:10 -0600

    Add gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
+    "gatsby-plugin-netlify",
     {
       resolve: "gatsby-plugin-sitemap",
       options: {
