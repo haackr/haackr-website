97151ab8a7e43149aff5235bdc3b16c8677e6bac
Ryan Haack <ryan@haackr.dev>
2023-08-27 11:50:08 -0600

    Create hero on index and change 404

---
+++ src/components/Layout.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: sans-serif;
+    font-size: 16px;
+  }
+`;
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <GlobalStyle />
+      {children}
+    </>
+  );
+};
+
+export default Layout;
