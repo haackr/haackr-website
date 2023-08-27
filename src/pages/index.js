32bd6ff7904c0b3d599a4b5438f390d3e91c20cb
Ryan Haack <ryan@haackr.dev>
2023-08-27 11:50:08 -0600

    Add some basic content to the home page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,8 @@ import styled from "styled-components";
 import Layout from "../components/Layout";
 
 const Hero = styled.div`
-  height: 75vh;
+  height: 100vh;
   display: flex;
-  align-items: center;
   max-width: 800px;
   margin: auto;
   line-height: 1.5;
@@ -18,6 +17,7 @@ const Hero = styled.div`
   }
 
   h1 {
+    margin-top: 25vh;
     font-size: 1.5rem;
   }
 
@@ -25,6 +25,24 @@ const Hero = styled.div`
     h1 {
       font-size: 2rem;
     }
+  }
+`;
+
+const HomeSection = styled.div`
+  max-width: 800px;
+  padding: 2rem;
+  margin: auto;
+
+  h2 {
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+
+  p {
+    line-height: 1.6;
+    color: #333;
+    max-width: 400px;
+    margin: auto;
   }
 `;
 
@@ -42,6 +60,26 @@ const IndexPage = () => {
             </div>
           </Hero>
         </header>
+        <article>
+          <HomeSection>
+            <h2>About Me</h2>
+            <p>
+              I'm a full stack developer from Albuquerque, New Mexico. I've been
+              programming since I was a kid messing around with BASIC. I'm
+              currently employed at Albuquerque Public Schools doing some custom
+              software development and administration of our capital project
+              management software e-Builder. In my free time I enjoy hiking,
+              climbing, backpacking, playing guitar, and cooking.
+            </p>
+          </HomeSection>
+        </article>
+        <HomeSection>
+          <h2>Contact Me</h2>
+          <p>
+            I'd love to help you build your project! Shoot me an email at{" "}
+            <a href="mailto:ryan@haackr.com">ryan@haackr.com</a>.
+          </p>
+        </HomeSection>
       </main>
     </Layout>
   );
